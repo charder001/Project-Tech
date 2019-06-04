ed54bfd6046ad9d5def5d3a81656620b9f3f63cf
charder001 <43436118+charder001@users.noreply.github.com>
2019-06-04 08:57:16 +0200

    Add files via upload
    Bugfixing, added routes and improved sessions

--- index.js
+++ index.js
@@ -9,132 +9,151 @@ var mongojs = require("mongojs")
 var mongoose = require('mongoose')
 var db = mongojs("MotoMatch", ["users"])
 var ObjectId = mongojs.ObjectID
-mongoose.connect('mongodb://' + "localhost" + '/' + "MotoMatch",
- { useNewUrlParser: true })
+mongoose.connect('mongodb://' + "localhost" + '/' + "MotoMatch", {
+  useNewUrlParser: true
+})
 
 
 var Schema = mongoose.Schema
 var userSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    password: String
+  firstName: String,
+  lastName: String,
+  password: String
 })
 
 var User = mongoose.model("users", userSchema)
 
 express()
-.use(express.static("static"))
-.use(bodyParser.urlencoded({extended: true}))
-.use(session({
+  .use(express.static("static"))
+  .use(bodyParser.urlencoded({
+    extended: true
+  }))
+  .use(session({
     resave: false,
     saveUninitialized: true,
-    secret: "Testcookie"/*process.env.SESSION_SECRET*/,
+    secret: "Testcookie" /*process.env.SESSION_SECRET*/ ,
     maxAge: 1000 * 60 * 60
-}))
+  }))
 
-.set("view engine", "ejs")
-.set("views", "view")
-.get("home", home)
-.get("/users", users)
-//.get("/:id", user)
-.get("/login", login)
-.get("/register", register)
-.post("/login", postlogin)
-.post("/users/add", submit)
-.post("/register", postregister)
-.post("out", out)
-.delete("/users/delete/:id", removeuser)
-.listen(3001, function(){
-    console.log("Server listening on port 3001")
+  .set("view engine", "ejs")
+  .set("views", "view")
+  .get("home", home)
+  .get("/users", users)
+  .get("/login", login)
+  .get("/register", register)
+  .get("/dashboard", dashboard)
+  .get("/signout", signout)
+  .post("/login", postlogin)
+  .post("/users/add", submit)
+  .post("/register", postregister)
+  .delete("/users/delete/:id", removeuser)
+  .listen(3007, function () {
+    console.log("Server listening on port 3007")
+  })
+
+function home(req, res) {
+
+}
+
+function dashboard(req, res) {
+  if (!req.session.user) {
+    return res.status(401).redirect("login")
+  }
+  db.users.find(function (err, docs) {
+  return res.status(200).render("dashboard.ejs", {
+    users: docs
+  })
 })
-
-
-
-function home(req, res){
-    console.log("nice")
 }
 
-function submit(req, res){
-    
-    var id = slug(req.body.firstName).toLowerCase()
-    var newUser = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        password: req.body.password
-    }
-    db.users.insert(newUser, function(err, result){
-        res.redirect("/users")
-    })
+function submit(req, res) {
+
+  var id = slug(req.body.firstName).toLowerCase()
+  var newUser = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    password: req.body.password
+  }
+  db.users.insert(newUser, function (err, result) {
+    res.redirect("/login")
+  })
 }
 
-function user(req, res){
-    db.users.find(function (err, docs) {
-    res.render("user.ejs", {users: docs})
+function users(req, res) {
+  db.users.find(function (err, docs) {
+    res.render("users.ejs", {
+      users: docs
     })
-}
-
-function users(req, res){
-    db.users.find(function (err, docs) {
-    res.render("users.ejs", {users: docs})
-    })
-}
-
-function removeuser(req, res){
-    db.users.remove({_id: ObjectId(req.params.id)}, function(err, result){
-      if (err){
-          console.log(err)
-      }  
-      res.redirect("/users")
-    })
+  })
 }
 
 function login(req, res) {
-    res.render('login.ejs' )
-
+  res.render('login.ejs')
 }
 
-
-function register(req, res){
-    
+function register(req, res) {
+  res.render("register.ejs")
 }
 
-function postregister(req, res){
-    var firstName = req.body.firstName
-    var lastName = req.body.lastName
-    var password = req.body.password
+function postregister(req, res) {
+  var firstName = req.body.firstName
+  var lastName = req.body.lastName
+  var password = req.body.password
 
-    var newuser = new User()
-    newuser.firstName = firstName
-    newuser.lastName = lastName
-    newuser.password = password
-    newuser.save(function(err, savedUser){
-      if(err){
-        console.log(err)
-        return res.status(500).send()
-      }
+  var newuser = new User()
+  newuser.firstName = firstName
+  newuser.lastName = lastName
+  newuser.password = password
+  newuser.save(function (err, savedUser) {
+    if (err) {
+      console.log(err)
+      return res.status(500).send()
+    }
 
-      return res.status(200).send()
-      
-    })
-    res.redirect("/users")
+    return res.status(200).redirect("/login")
+
+  })
 }
 
-function out(req, res){
-
-}
-
-function postlogin(req, res){
+function postlogin(req, res) {
   var firstName = req.body.firstName
   var password = req.body.password
 
-  user.findOne({firstName: firstName, password: password}, function(err, user){
-    if (err){
+  User.findOne({
+    firstName: firstName,
+    password: password
+  }, function (err, user) {
+    if (err) {
       console.log(err)
-    } if(!user){
-      console.log("login unsuccesful")
-      return res.status(404).send()
+      return res.status(500).send()
     }
+    if (!user) {
+      console.log("login unsuccessful")
+      return res.status(404).redirect("/login")
+    }
+    req.session.user = user;
     console.log("login succesful")
-    return res.status(200).send()
+    res.redirect("/dashboard")
+    res.status(200).send()
+  }) 
+}
+
+
+
+function signout(req, res){
+  req.session.destroy()
+  console.log("signed out")
+  res.redirect("/login")
+  return res.status(200).send()
+}
+
+function removeuser(req, res) {
+  db.users.remove({
+    _id: ObjectId(req.params.id)
+  }, function (err, result) {
+    if (err) {
+      console.log(err)
+    }
+    res.redirect("/dashboard")
   })
 }
