5357b4ffab9128d0f4fcbf97bd290934ee31c79a
charder001 <43436118+charder001@users.noreply.github.com>
2019-06-19 22:40:59 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+{
+    "parserOptions": {
+        "ecmaVersion": 5,
+        "sourceType": "module"
+    },
+    "rules": {
+        "semi": "error",
+        "no-extra-semi": "error",
+        "quotes": ["error", "single"],
+        "camelcase": "error"
+    }
+}
